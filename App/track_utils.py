8ee85d9f4a0495f039737eb5ce5931d2b97cb444
Vin <vinijilljill@yahoo.com>
2021-09-04 14:57:57 +0800

    Update track_utils.py

--- App/track_utils.py
+++ App/track_utils.py
@@ -1,1 +1,30 @@
+# Load Database Packages
+import sqlite3
+conn sqlite3.connect('data.db')
+c = conn.cursor()
 
+# Database Functions
+def create_page_visited_table():
+    c.execute('CREATE TABLE IF NOT EXISTS pageTrackTable(pagename TEXT, timeOfVisit TIMESTAMP)')
+
+def add_page_visited_details(pagename, timeOfVisit):
+    c.execute('INSERT INTO pageTrackTable(pagename, timeOfVisit) VALUES(?,?)', (pagename,timeOfVisit))
+    conn.commit()
+
+def view_all_page_visited_details():
+    c.execute('SELECT * FROM pageTrackTable')
+    data = c.fetchall()
+    return data
+
+# Function to Track Input and Prediction
+def create_emotionclf_table():
+    c.execute('CREATE TABLE IF NOT EXISTS emotionClfTable(rawtext TEXT, prediction TEXT, probability NUMBER, timeOfVisit TIMESTAMP)')
+
+def add_prediction_details(rawtext, prediction, probability, timeOfVisit):
+    c.execute('INSERT INTO emotionClfTable(rawtext, prediction, probability, timeOfVisit) VALUES(?,?,?,?)',(rawtext, prediction, probability, timeOfVisit))
+    conn.commit()
+
+def view_all_prediction_details():
+    c.execute('SELECT * FROM emotionClfTable')
+    data = c.fetchall()
+    return data
