7fcbfe7a32656fb49b1552346c27d7b964d8d460
Vin <vinijilljill@yahoo.com>
2021-09-04 15:26:54 +0800

    Update app.py

--- App/app.py
+++ App/app.py
@@ -1,16 +1,23 @@
-# Core Packages
+# ------ Core Packages ------
 import streamlit as st
 import altair as alt
+import plotly.express as px
 
-# EDA Packages
+# ------ EDA Packages ------
 import pandas as pd
 import numpy as np
+from datetime import datetime
 
-# Utils
+# ------ Utils ------
 import joblib
 
-# Functions to read emotions
+# Read emotions
 pipe_lr = joblib.load(open("models/emotion_classifier_pipe_lr_3_sept_2021.pkl", "rb"))
+
+# Track Utils
+from track_utils import create_page_visited_table,add_page_visited_details,view_all_page_visited_details,add_prediction_details,view_all_prediction_details,create_emotionclf_table
+
+# ------ Functions ------
 
 # Function to predict emotions
 def predict_emotions(docx):
@@ -30,8 +37,15 @@ def main():
     menu = ['Home', 'Monitor', 'About']
     choice = st.sidebar.selectbox("Menu", menu)
 
+    # Start creating tables for new inputs and emotions_emoji_dict
+    create_emotionclf_table()
+    create_page_visited_table()
+
     if choice == "Home":
+        add_page_visited_details("Home", datetime.now())
+
         st.subheader("Home - Emotion In Text")
+
         with st.form(key = 'emotion_clf_form'):
             raw_text = st.text_area("Type here")
             submit_text = st.form_submit_button(label = "Submit")
@@ -42,6 +56,8 @@ def main():
                 # Apply prediction functions here
                 prediction = predict_emotions(raw_text)
                 probability = get_prediction_proba(raw_text)
+
+                add_prediction_details(raw_text, prediction, np.max(probability), datetime.now())
 
                 with col1:
                     st.success("Original Text")
@@ -64,9 +80,31 @@ def main():
                     st.altair_chart(fig, use_container_width = True)
 
     elif choice == "Monitor":
+        add_page_visited_details("Monitor", datetime.now())
         st.subheader("Monitor App")
+
+        with st.beta_expander("Page Metrics"):
+            page_visited_details = pd.DataFrame(view_all_page_visited_details(), columns = ['Pagename', 'Time_Of_Visit'])
+            st.dataframe(page_visited_details)
+
+            pg_count = page_visited_details['Pagename'].value_counts().rename_axis('Pagename').reset_index(name = 'Counts')
+            c = alt.Chart(pg_count).mark_bar().encode(x = 'Pagename', y = 'Counts', color = 'Pagename')
+            st.altair_chart(c, use_container_width = True)
+
+            p = px.pie(pg_count, values = 'Counts', names = 'Pagename')
+            st.plotly_chart(p, use_container_width = True)
+
+        with st.beta_expander('Emotion Classifier Metrics'):
+            df_emotions = pd.DataFrame(view_all_prediction_details(), columns = ['Rawtext', 'Prediction', 'Probability', 'Time_Of_Visit'])
+            st.dataframe(df_emotions)
+
+            prediction_count = df.emotions['Prediction'].value_counts().rename_axis('Prediction').reset_index(name = 'Counts')
+            pc = alt.Chart(prediction_count).mark_bar().encode(x = 'Prediction', y = 'Counts', color = 'Prediction')
+            st.altair_chart(pc, use_container_width = True)
+
     else:
         st.subheader("About")
+        add_page_visited_details("About", datetime.now())
 
 if __name__ == '__main__':
     main()
