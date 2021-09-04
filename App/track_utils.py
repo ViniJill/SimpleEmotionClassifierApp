b63ed1b0d8ee7c97d6b9600a10f9ccc712bffe89
Vin <vinijilljill@yahoo.com>
2021-09-04 15:44:26 +0800

    Completed simple emotion detection app

--- App/track_utils.py
+++ App/track_utils.py
@@ -1,30 +1,31 @@
 # Load Database Packages
 import sqlite3
-conn sqlite3.connect('data.db')
+conn = sqlite3.connect('data.db')
+conn = sqlite3.connect('data.db', check_same_thread = False)
 c = conn.cursor()
 
 # Database Functions
 def create_page_visited_table():
-    c.execute('CREATE TABLE IF NOT EXISTS pageTrackTable(pagename TEXT, timeOfVisit TIMESTAMP)')
+	c.execute('CREATE TABLE IF NOT EXISTS pageTrackTable(pagename TEXT,timeOfvisit TIMESTAMP)')
 
-def add_page_visited_details(pagename, timeOfVisit):
-    c.execute('INSERT INTO pageTrackTable(pagename, timeOfVisit) VALUES(?,?)', (pagename,timeOfVisit))
-    conn.commit()
+def add_page_visited_details(pagename,timeOfvisit):
+	c.execute('INSERT INTO pageTrackTable(pagename,timeOfvisit) VALUES(?,?)',(pagename,timeOfvisit))
+	conn.commit()
 
 def view_all_page_visited_details():
-    c.execute('SELECT * FROM pageTrackTable')
-    data = c.fetchall()
-    return data
+	c.execute('SELECT * FROM pageTrackTable')
+	data = c.fetchall()
+	return data
 
 # Function to Track Input and Prediction
 def create_emotionclf_table():
-    c.execute('CREATE TABLE IF NOT EXISTS emotionClfTable(rawtext TEXT, prediction TEXT, probability NUMBER, timeOfVisit TIMESTAMP)')
+	c.execute('CREATE TABLE IF NOT EXISTS emotionclfTable(rawtext TEXT,prediction TEXT,probability NUMBER,timeOfvisit TIMESTAMP)')
 
-def add_prediction_details(rawtext, prediction, probability, timeOfVisit):
-    c.execute('INSERT INTO emotionClfTable(rawtext, prediction, probability, timeOfVisit) VALUES(?,?,?,?)',(rawtext, prediction, probability, timeOfVisit))
-    conn.commit()
+def add_prediction_details(rawtext,prediction,probability,timeOfvisit):
+	c.execute('INSERT INTO emotionclfTable(rawtext,prediction,probability,timeOfvisit) VALUES(?,?,?,?)',(rawtext,prediction,probability,timeOfvisit))
+	conn.commit()
 
 def view_all_prediction_details():
-    c.execute('SELECT * FROM emotionClfTable')
-    data = c.fetchall()
-    return data
+	c.execute('SELECT * FROM emotionclfTable')
+	data = c.fetchall()
+	return data
